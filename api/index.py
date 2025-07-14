7d3ae02ca8e08cb28ab8af3a7a937e33dbba0130
zuhaira-hilal-nayyara <pkm56@teknokrat.ac.id>
2025-07-14 16:25:20 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id('14EXDQFZ3w3m20W1w3EBl0MGCO9hxNz63ZWhC4tQewYY')
+config_artikel.sheet_id('10wYgfG22udO0nQ6NCW3oHXdDpYXPaULn_dgoRlp0jVg')
 config_artikel.gid('710523043')
 
 
